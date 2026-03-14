d7f3fa2a93dbbb6367f2ccf100656383ff073dcb
SVIKI17 <sviki200629@gmail.com>
2026-03-14 14:40:56 +0300

    Добавил геттеры, сеттеры и меню управления ресурсами

--- classes/Machine.dart
+++ classes/Machine.dart
@@ -1,11 +1,53 @@
 class Machine {
-  int _coffeeBeans = 500;
+  int _coffeeBeans = 500;  
   int _milk = 1000;        
   int _water = 2000;       
   int _cash = 0;           
 
   Machine();
-  
+
+  int get coffeeBeans => _coffeeBeans;
+  int get milk => _milk;
+  int get water => _water;
+  int get cash => _cash;
+
+  void addCoffeeBeans(int amount) {
+    if (amount > 0) {
+      _coffeeBeans += amount;
+      print('Добавлено $amount гр кофейных зерен. Теперь: $_coffeeBeans гр');
+    }
+  }
+
+  void addMilk(int amount) {
+    if (amount > 0) {
+      _milk += amount;
+      print('Добавлено $amount мл молока. Теперь: $_milk мл');
+    }
+  }
+
+  void addWater(int amount) {
+    if (amount > 0) {
+      _water += amount;
+      print('Добавлено $amount мл воды. Теперь: $_water мл');
+    }
+  }
+
+  void addCash(int amount) {
+    if (amount > 0) {
+      _cash += amount;
+      print('Внесено $amount руб. В автомате: $_cash руб');
+    }
+  }
+
+  void withdrawCash() {
+    if (_cash > 0) {
+      print('Изъято $_cash руб');
+      _cash = 0;
+    } else {
+      print('В автомате нет денег');
+    }
+  }
+
   void showStatus() {
     print('\n=== СОСТОЯНИЕ КОФЕМАШИНЫ ===');
     print('Кофейные зерна: $_coffeeBeans гр');
--- main.dart
+++ main.dart
@@ -3,8 +3,80 @@ import 'classes/Machine.dart';
 
 void main() {
   Machine machine = Machine();
-
+  
   print('=== КОФЕМАШИНА ===');
   
-  machine.showStatus();
+  bool isRunning = true;
+  
+  while (isRunning) {
+    print('\n--- МЕНЮ ---');
+    print('1. Показать состояние машины');
+    print('2. Добавить кофейные зерна');
+    print('3. Добавить воду');
+    print('4. Добавить молоко');
+    print('5. Внести деньги');
+    print('6. Изъять деньги');
+    print('0. Выход');
+    stdout.write('Выберите действие: ');
+    
+    String? choice = stdin.readLineSync();
+    
+    switch (choice) {
+      case '1':
+        machine.showStatus();
+        break;
+        
+      case '2':
+        stdout.write('Сколько грамм кофе добавить? ');
+        int? amount = int.tryParse(stdin.readLineSync() ?? '');
+        if (amount != null && amount > 0) {
+          machine.addCoffeeBeans(amount);
+        } else {
+          print('Некорректное значение');
+        }
+        break;
+        
+      case '3':
+        stdout.write('Сколько мл воды добавить? ');
+        int? amount = int.tryParse(stdin.readLineSync() ?? '');
+        if (amount != null && amount > 0) {
+          machine.addWater(amount);
+        } else {
+          print('Некорректное значение');
+        }
+        break;
+        
+      case '4':
+        stdout.write('Сколько мл молока добавить? ');
+        int? amount = int.tryParse(stdin.readLineSync() ?? '');
+        if (amount != null && amount > 0) {
+          machine.addMilk(amount);
+        } else {
+          print('Некорректное значение');
+        }
+        break;
+        
+      case '5':
+        stdout.write('Сколько рублей внести? ');
+        int? amount = int.tryParse(stdin.readLineSync() ?? '');
+        if (amount != null && amount > 0) {
+          machine.addCash(amount);
+        } else {
+          print('Некорректное значение');
+        }
+        break;
+        
+      case '6':
+        machine.withdrawCash();
+        break;
+        
+      case '0':
+        isRunning = false;
+        print('До свидания!');
+        break;
+        
+      default:
+        print('Неизвестная команда');
+    }
+  }
 }
